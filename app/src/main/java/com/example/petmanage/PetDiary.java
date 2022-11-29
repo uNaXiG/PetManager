921bc5efaac2cfe135564e8ec9d355d2e6831d5b
uNaXiG <a0978626228a@gmail.com>
2022-11-30 02:44:01 +0800

    diary

--- app/src/main/java/com/example/petmanage/PetDiary.java
+++ app/src/main/java/com/example/petmanage/PetDiary.java
@@ -14,34 +14,53 @@ import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
 public class PetDiary extends AppCompatActivity {
 
     ListView diaryListView;
     Button addDiaryButton;
 
-    String Title[] = {"11/23","11/24","11/25","11/26","11/27"};
-    String sec[] = {"我是狗",
-            "我是兔子",
-            "我是貓",
-            "我也是狗",
-            "我還是狗"};
-    int image[] = {R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4,R.drawable.img5};
+    ShapeableImageView pet_icon;
+    TextView pet_name;
+    String title[] = {"【2022-11-25】哭啊","【2022-11-26】喜哩靠","【2022-11-28】好想贏韓國","【2022-11-29】世足"};
+    String contant[] = {"我想拿A",
+            "現在放棄，寒假就開始了",
+            "怎樣都要畢業吧",
+            "C羅攜鑼去西螺吸螺XD"};
+
+    int image[] = {R.drawable.giwawa,R.drawable.peko_img,R.drawable.bg_img,R.drawable.show_pet_pic};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_diary);
 
+
+
+
         diaryListView = (ListView) findViewById(R.id.diary_list);
         addDiaryButton = (Button) findViewById(R.id.add_diary_button);
 
-        MyAdapter myAdapter = new MyAdapter(this,Title,sec,image);
+        MyAdapter myAdapter = new MyAdapter(this, title, contant, image);
         diaryListView.setAdapter(myAdapter);
+        diaryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String title_str = title[(int) id];
+                Toast.makeText(PetDiary.this, title_str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         transparentStatusBar();
     }
 
@@ -76,10 +95,10 @@ public class PetDiary extends AppCompatActivity {
 
             return row;
         }
+
+
+
     }
-
-
-
 
 
     private void transparentStatusBar() {
